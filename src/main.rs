763184dae625c12835206245d1a7e2d231494333
Moca114514 <1043530132@qq.com>
2025-03-06 13:28:14 +0800

    docs:添加了部分注释

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![no_main]
+// 不再使用 Rust 标准库 std，转而使用核心库 core
+// 核心库 core 可以直接在裸机上使用，但 std 不能，需要 OS 的支持
 #![no_std]
 
 use core::arch::global_asm;
@@ -11,6 +13,8 @@ mod sync;
 mod trap;
 mod syscall;
 
+// include_str! 宏的作用是将文件内容作为字符串常量嵌入到程序
+// global_asm! 宏的作用是将汇编代码嵌入到程序中
 global_asm!(include_str!("entry.S"));
 global_asm!(include_str!("link_app.S"));
 
--- src/panic.rs
+++ src/panic.rs
@@ -2,6 +2,7 @@ use core::panic::PanicInfo;
 use sbi::shutdown;
 use crate::println;
 
+// 用于标记 core 中的 panic! 宏要对接的函数
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
