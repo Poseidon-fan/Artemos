1f868d19203c1585bc55ffe9e4d12405ac4f38e5
Moca114514 <1043530132@qq.com>
2025-03-25 19:35:03 +0800

    feat: 增加了 frame_allocator

--- src/mm/address.rs
+++ src/mm/address.rs
@@ -1,6 +1,7 @@
 use core::fmt;
 use core::fmt::{Debug, Formatter};
 use crate::config::*;
+use crate::mm::page_table::PageTableEntry;
 
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct PhysAddr(pub usize);
@@ -36,17 +37,6 @@ impl From<usize> for PhysAddr {
 }
 impl From<usize> for PhysPageNum {
     fn from(v: usize) -> Self { Self(v & ( (1 << PPN_WIDTH_SV39) - 1 )) }
-}
-
-impl From<usize> for PhysAddr {
-    fn from(v: usize) -> Self {
-        Self(v & ((1 << PA_WIDTH_SV39) - 1))
-    }
-}
-impl From<usize> for PhysPageNum {
-    fn from(v: usize) -> Self {
-        Self(v & ((1 << PPN_WIDTH_SV39) - 1))
-    }
 }
 impl From<usize> for VirtAddr {
     fn from(v: usize) -> Self {
@@ -140,5 +130,20 @@ impl From<PhysAddr> for PhysPageNum {
 impl From<PhysPageNum> for PhysAddr {
     fn from(v: PhysPageNum) -> Self {
         Self(v.0 << PAGE_SIZE_BITS)
+    }
+}
+
+impl PhysPageNum {
+    pub fn get_pte_array(&self) -> &'static mut [PageTableEntry] {
+        let pa: PhysAddr = (*self).into();
+        unsafe { core::slice::from_raw_parts_mut(pa.0 as *mut PageTableEntry, 512) }
+    }
+    pub fn get_bytes_array(&self) -> &'static mut [u8] {
+        let pa: PhysAddr = (*self).into();
+        unsafe { core::slice::from_raw_parts_mut(pa.0 as *mut u8, 4096) }
+    }
+    pub fn get_mut<T>(&self) -> &'static mut T {
+        let pa: PhysAddr = (*self).into();
+        unsafe { (pa.0 as *mut T).as_mut().unwrap() }
     }
 }
--- src/mm/mod.rs
+++ src/mm/mod.rs
@@ -3,8 +3,10 @@ use crate::mm::heap_allocator::init_heap;
 mod heap_allocator;
 mod address;
 mod page_table;
+mod frame_allocator;
 
 pub fn heap_test() {
     heap_allocator::init_heap();
     heap_allocator::heap_test();
+    frame_allocator::frame_allocator_test();
 }
