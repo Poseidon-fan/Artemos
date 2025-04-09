feef104ad43d4122f74b480e38a1f7b992b9c1c3
Moca114514 <1043530132@qq.com>
2025-04-09 10:39:14 +0800

    feat: update linker and loader

--- build.rs
+++ build.rs
@@ -38,6 +38,10 @@ _num_app:
     }
     writeln!(f, r#"    .quad app_{}_end"#, apps.len() - 1)?;
 
+    for app in apps.iter() {
+        writeln!(f, r#"    .string "{}""#, app)?;
+    }
+
     for (idx, app) in apps.iter().enumerate() {
         println!("app_{}: {}", idx, app);
         writeln!(
