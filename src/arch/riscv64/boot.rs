6b3607838f2604ee25c74a9993937d351a4045f2
fanxingkun <18646154381@163.com>
2025-05-15 16:29:32 +0800

    feat: read smp from dt

--- src/arch/riscv64/boot.rs
+++ src/arch/riscv64/boot.rs
@@ -3,6 +3,7 @@ use core::{
     sync::atomic::{AtomicBool, Ordering},
 };
 
+use fdt::Fdt;
 use log::info;
 
 use crate::{
@@ -15,18 +16,25 @@ global_asm!(include_str!("entry.asm"));
 static FIRST_HART: AtomicBool = AtomicBool::new(true);
 
 #[unsafe(no_mangle)]
-pub fn kernel_main(hartid: usize, _device_tree_paddr: usize) -> ! {
+pub fn kernel_main(hartid: usize, device_tree_paddr: usize) -> ! {
     if FIRST_HART
         .compare_exchange(true, false, Ordering::SeqCst, Ordering::SeqCst)
         .is_ok()
     {
         logging::init();
         info!("hart: {} is starting", hartid);
-        for i in 0..4 {
-            if i != hartid {
-                sbi::start_hart(i, 0x80200000, 0);
+        // get hart count from device tree
+        let fdt = unsafe { Fdt::from_ptr(device_tree_paddr as *const u8).unwrap() };
+        let mut hart_count = 0;
+        for node in fdt.find_all_nodes("/cpus/cpu") {
+            if let Some(_reg) = node.property("reg") {
+                hart_count += 1;
             }
         }
+        info!("hart_count: {}", hart_count);
+        (0..hart_count).filter(|&i| i != hartid).for_each(|i| {
+            sbi::start_hart(i, 0x80200000, 0);
+        });
         loop {}
     } else {
         others_main(hartid)
--- src/arch/riscv64/sbi/hsm.rs
+++ src/arch/riscv64/sbi/hsm.rs
@@ -9,6 +9,9 @@ const FID_HART_GET_STATUS: usize = 2;
 const FID_HART_SUSPEND: usize = 3;
 
 /// start a hart
+/// note: mark as `#[inline(never)]` to avoid the compiler to optimize the function call,
+/// otherwise, there will be issues when `start_hart` is called in a loop in `boot.rs`.
+#[inline(never)]
 pub fn start_hart(hartid: usize, start_addr: usize, opaque: usize) -> usize {
     sbi_call(EID_HSM, FID_HART_START, hartid, start_addr, opaque)
 }
