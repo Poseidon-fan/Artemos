7ce317e8d8990d280ef19fd0272c56a1a9e6f71f
MitakeMoca <1043530132@qq.com>
2025-05-17 22:34:07 +0800

    feat: locked queue allocator

--- src/arch/riscv64/utils/queue_allocator.rs
+++ src/arch/riscv64/utils/queue_allocator.rs
@@ -1,6 +1,12 @@
 use alloc::vec::Vec;
 
+use spin::Mutex;
+
 pub struct QueueAllocator {
+    inner: Mutex<QueueAllocatorInner>,
+}
+
+struct QueueAllocatorInner {
     current: usize,
     recycled: Vec<usize>,
 }
@@ -8,22 +14,25 @@ pub struct QueueAllocator {
 impl QueueAllocator {
     pub fn new() -> Self {
         QueueAllocator {
-            current: 0,
-            recycled: Vec::new(),
+            inner: Mutex::new(QueueAllocatorInner {
+                current: 0,
+                recycled: Vec::new(),
+            }),
         }
     }
 
-    pub fn alloc(&mut self) -> usize {
-        if let Some(id) = self.recycled.pop() {
+    pub fn alloc(&self) -> usize {
+        let mut inner = self.inner.lock();
+        if let Some(id) = inner.recycled.pop() {
             id
         } else {
-            self.current += 1;
-            self.current - 1
+            inner.current += 1;
+            inner.current - 1
         }
     }
 
-    // RAII: dealloc is called when the object goes out of scope.
-    pub fn dealloc(&mut self, id: usize) {
-        self.recycled.push(id);
+    pub fn dealloc(&self, id: usize) {
+        let mut inner = self.inner.lock();
+        inner.recycled.push(id);
     }
 }
